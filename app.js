6695bbc7848cee551bf656e1429d6ca1f3a61058
unknown <tayanesilvasouzas@gmail.com>
2020-12-28 01:53:27 -0300

    atualizando hora

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-const nextYear = new Date().getFullYear() + 1 
+const secondsContainer = document.querySelector('#seconds')
+const minutesContainer = document.querySelector('#minutes')
+const hoursContainer = document.querySelector('#hours')
+const daysContainer = document.querySelector('#days')
 
+
+
+const nextYear = new Date().getFullYear() + 1 
 const newYearTime = new Date(`january 01 ${nextYear} 00:00:00`)
 
-console.log(newYearTime)
+
+ 
+const updateCountdown = () => {
+    const currentTime = new Date ()
+    const difference = newYearTime - currentTime
+    const days = Math.floor(difference / 1000 / 60 / 60 / 24)
+    const hours = Math.floor(difference / 1000 / 60 / 60) % 24
+    const minutes = Math.floor(difference / 1000 / 60) % 60 
+    const seconds = Math.floor(difference / 1000) % 60 
+
+    secondsContainer.textContent = seconds < 10 ? '0' + seconds : seconds
+    minutesContainer.textContent = minutes < 10 ? '0' + minutes : minutes
+    hoursContainer.textContent = hours < 10 ? '0' + hours : hours
+    daysContainer.textContent = days < 10 ? '0' + days : days
+}
+
+
+setInterval(updateCountdown, 1000)
